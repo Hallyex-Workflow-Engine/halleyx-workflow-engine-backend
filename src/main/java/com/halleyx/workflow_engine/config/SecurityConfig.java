c1f6b73112022b6014c16012815e6b034a574c09
Nandha Kishor <nksiva2005@gmail.com>
2026-03-23 05:23:13 +0530

    port

--- src/main/java/com/halleyx/workflow_engine/config/SecurityConfig.java
+++ src/main/java/com/halleyx/workflow_engine/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     var config = new org.springframework.web.cors.CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:5174"));
+                    config.setAllowedOrigins(List.of("http://localhost:5173"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
